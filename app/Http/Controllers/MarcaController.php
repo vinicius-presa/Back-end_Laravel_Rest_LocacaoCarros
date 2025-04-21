6aeb487a4ce8da56b0f34f78f6d9cdf37df0cc63
Vinicius Presa <viniciuspre@gmail.com>
2025-04-21 00:16:12 -0300

    feat:: implementado API webservice Rest para o resource Marca

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -2,84 +2,110 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use App\Models\Marca;
 use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
+    public $marca;
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
+    /*
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        $marcas = $this->marca->all();
+        return response()->json($marcas, 200);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
     /**
      * Store a newly created resource in storage.
-     *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+        //stateless
+        $image = $request->file('imagem');
+        $imagem_urn = $image->store('imagens', 'public');
+        
+        $marca = $this->marca->create([
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn   
+        ]);
+        return response()->json($marca, 201);
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
-        //
+        $marca = $this->marca->find($id);
+        if($marca === null){
+            return response()->json(['Erro' => 'Recurso Pesquisado não Existe'], 404);
+        }
+        return response()->json($marca, 200);
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Marca $marca)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
+    /*
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
-        //
-    }
+        $marca = $this->marca->find($id);
+        if($marca === null){
+            return response()->json(['Erro' => 'Recurso solicidado não Existe'], 404);
+        }
+        //percorre todas a regras aplicadas pelo model
+        if($request->method() === 'PATCH'){
+            $regrasDinamicas = array();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Marca  $marca
+            foreach ($marca->rules() as $input => $regras) {
+                //coletar regras aplicaveis
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regras; 
+                }
+
+            }
+            $request->validate($regrasDinamicas, $marca->feedback());
+        }else{
+            $request->validate($marca->rules(), $marca->feedback());
+        }
+        //remove o arquivo antigo caso um novo arquivo seja enviado no request
+        if($request->file('imagem')){
+            Storage::disk('public')->delete($marca->imagem);
+        }
+
+        $image = $request->file('imagem');
+        $imagem_urn = $image->store('imagens', 'public');
+
+        $marca->update([
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn   
+        ]);
+        return response()->json($marca, 200);
+    }
+    /*
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
-        //
+        $marca = $this->marca->find($id);
+        if($marca === null){
+            return response()->json(['Erro' => 'Impossível realizar a exclusão. Recurso solicidado não Existe'], 404);
+        }
+        //remove o arquivo antigo caso um novo arquivo seja enviado no request
+        Storage::disk('public')->delete($marca->imagem);
+       
+        $marca->delete();
+        return response()->json('A marca foi removida com sucesso!', 200);
     }
 }
--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Marca extends Model
 {
     use HasFactory;
+    protected $fillable = ['nome','imagem'];
+
+    public function rules() {
+        return [
+            'nome' => 'required|unique:marcas,nome,'.$this->id.'|min:3',
+            'imagem' => 'required|file|mimes:png'
+        ];
+    }
+
+    public function feedback(){
+        return [
+            'required' => 'O campo :attribute é obrigatorio',
+            'imagem.mines' => 'O arquivo tem que ser PNG',
+            'nome.unique' => 'O nome da marca ja existe',
+            'nome.min' => 'O nome deve ter no minimo 3 caracteres'
+        ];
+    }
+
 }
+
